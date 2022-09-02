a773f25ef431947f2d8d207d5b4042131d98f882
NeoLez <leogonzachi@gmail.com>
2022-09-02 10:06:47 -0300

    test

--- src/main/java/net/fabricmc/custom_ambiance/Client.java
+++ src/main/java/net/fabricmc/custom_ambiance/Client.java
@@ -23,7 +23,7 @@ public class Client implements ClientModInitializer {
 	public static final Identifier SPARROW_CHIRP_IDENTIFIER = new Identifier(MODID,"sparrow_chirp");
 	public static final SoundEvent SPARROW_CHIRP = new SoundEvent(SPARROW_CHIRP_IDENTIFIER);
 
-	@Overrides
+	@Override
 	public void onInitializeClient() {
 
 		Registry.register(Registry.SOUND_EVENT, SPARROW_CHIRP_IDENTIFIER, SPARROW_CHIRP);
